20ac0de2153bf4c2673b9862b1685cbef114e4f8
shaswat785 <shaswat785@gmail.com>
2026-01-11 19:13:43 +0000

    - DATABASE_URL

--- core/db.py
+++ core/db.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 from core.config import settings
 
-engine = create_engine(settings.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(settings.DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
