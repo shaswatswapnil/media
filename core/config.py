257ae7824bcc506564956f67b0b629d9b5a2bac0
shaswat785 <shaswat785@gmail.com>
2026-01-13 12:03:43 +0000

    -----

--- core/config.py
+++ core/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8  # 8 days
+    ALGORITHM: str = "HS256"
 
     class Config:
         env_file = ".env"
